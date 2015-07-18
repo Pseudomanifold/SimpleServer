443ee24c4f3718c261c2045d870cff7227fcd545
Bastian Rieck <bastian@rieck.ru>
2015-07-18 22:19:57 +0200

    Modified QOTD server to return a random quote

--- qotd.cc
+++ qotd.cc
@@ -3,6 +3,11 @@
 
 #include <signal.h>
 
+#include <fstream>
+#include <random>
+#include <string>
+#include <vector>
+
 Server server;
 
 void handleExitSignal( int /* signal */ )
@@ -10,15 +15,56 @@ void handleExitSignal( int /* signal */ )
   server.close();
 }
 
-int main( int /* argc */, char** /* argv */ )
+void unescapeQuote( std::string& quote )
 {
+  std::size_t position    = 0;
+  std::string target      = "\\n";
+  std::string replacement = "\n";
+  while( ( position = quote.find( target, position ) ) != std::string::npos )
+  {
+    quote.replace( position, target.length(), replacement );
+    position += replacement.length();
+  }
+}
+
+std::vector<std::string> readQuotes( const std::string& filename )
+{
+  std::ifstream in( filename );
+  if( !in )
+    return {};
+
+  std::vector<std::string> quotes;
+  std::string line;
+
+  while( std::getline( in, line ) )
+  {
+    unescapeQuote( line );
+    quotes.push_back( line );
+  }
+
+  return quotes;
+}
+
+int main( int argc, char** argv )
+{
+  std::vector<std::string> quotes;
+
+  if( argc > 1 )
+    quotes = readQuotes( argv[1] );
+  else
+    quotes = { "Sorry, no quote today, mate.\n" };
+
+  std::random_device rd;
+  std::mt19937 rng( rd() );
+  std::uniform_int_distribution<std::size_t> distribution( 0, quotes.size() - 1 );
+
   signal( SIGINT, handleExitSignal );
 
   server.setPort( 2048 );
 
-  server.onAccept( [] ( std::unique_ptr<ClientSocket> socket )
+  server.onAccept( [&] ( std::unique_ptr<ClientSocket> socket )
   {
-    socket->write( "Sorry, no quote today, mate.\n" );
+    socket->write( quotes.at( distribution( rng ) ) );
     socket->close();
   } );
 
