cc745579e7425f561086e30df4f67610ef77a6c5
Bastian Rieck <bastian@rieck.ru>
2015-08-09 22:01:44 +0200

    Implemented simple echo server
    For some reason, shutting down the server does not yet work as
    expected. Apart from that, the protocols seems to work.

---
+++ echo.cc
@@ -0,0 +1,42 @@
+#include "ClientSocket.hh"
+#include "Server.hh"
+
+#include <signal.h>
+
+#include <fstream>
+#include <random>
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+Server server;
+
+void handleExitSignal( int /* signal */ )
+{
+  server.close();
+}
+
+int main( int argc, char** argv )
+{
+  signal( SIGINT, handleExitSignal );
+
+  server.setPort( 1031 );
+
+  server.onAccept( [&] ( std::weak_ptr<ClientSocket> /* socket */ )
+  {
+  } );
+
+  server.onRead( [&] ( std::weak_ptr<ClientSocket> socket )
+  {
+    if( auto s = socket.lock() )
+    {
+      auto data = s->read();
+      s->write( data );
+    }
+  } );
+
+  server.listen();
+
+  return 0;
+}
